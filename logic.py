7ce3fc6dcc35d2c6299e8b4a2bc4c6766fecda4b
Kunal <kunaltyagi760@gmail.com>
2024-01-22 23:39:54 +0530

    Updated logic

--- logic.py
+++ logic.py
@@ -17,4 +17,34 @@ def height(node):
         
     # optimize sol -1 
     
-   
+    def f1(root):
+        if root == None:
+            return [True,0]
+            
+        lhs = f1(root.left)
+        rhs = f1(root.right)
+        
+        balance = lhs[0] and rhs[0] and abs(lhs[1]-rhs[1]) <= 1
+        
+        return [balance,1+max(rhs[1],lhs[1])]
+        
+    # optimize sol -2
+    
+    def f2(root):
+        if root == None:
+            return True
+            
+        lh = f2(root.left)
+        if lh==0:
+            return 0
+            
+        rh = f2(root.right)
+        if rh==0:
+            return 0
+            
+        if abs(rh-lh) > 1:
+            return 0
+        else:
+            return 1+max(rh,lh)
+        
+    return f2(root) != 0
