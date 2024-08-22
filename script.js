b56e4496a6334cfe8adb30109c4101b70c34749f
Kunal <kunaltyagi760@gmail.com>
2024-08-22 11:38:28 +0530

    resume link updated again

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     function downloadResume() {
         
-        const resumePath = 'https://drive.google.com/file/d/1pmPQ2iBPNd1A1gq4rELm06PFukwqhkYu/view?usp=sharing';
+        const resumePath = 'https://drive.google.com/file/d/1UgIFnTu94zLt4x0s51sZWLcc_ByDaDHl/view?usp=sharing';
 
         const link = document.createElement('a');
         link.href = resumePath;
